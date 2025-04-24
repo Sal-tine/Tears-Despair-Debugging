256533ba0107f81cd9a73713cfdd7cdbb51227e6
ygsaltine <ygsaltine@ygs-macbook-air>
2025-04-23 19:17:17 -0500

    implemented javaDoc for game class

--- SP2025_CS3330_HW4_G0/src/hw4/game/Game.java
+++ SP2025_CS3330_HW4_G0/src/hw4/game/Game.java
@@ -10,6 +10,10 @@ import hw4.player.Player;
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * The {@code Game} class controls the maze game logic by intializing a maze
+ * movement and play actions.
+ */
 
 public class Game{
 	
@@ -17,9 +21,21 @@ public class Game{
 	private static final int MinGrid = 3;
 	private static final int MaxGrid = 7;
 	
+	/**
+	 * Makes a game with grid
+	 * 
+	 * @param grid the grid to be used for the game
+	 */
 	public Game(Grid grid) {
 		this.grid = grid;
 	}
+	
+	/**
+	 * creates a game with a random size grid between 3 and 7.
+	 * if the grid is outside the range it'll be null
+	 * 
+	 * @param size the size of the grid
+	 */
 	
 	public Game(int size) {
 		if(size >= MinGrid && size <= MaxGrid) {
@@ -29,13 +45,30 @@ public class Game{
 		}
 	}
 	
+	
+	/**
+	 * returns current grid.
+	 * @return the games grid
+	 */
 	 public Grid getGrid() {
 	        return grid;
 	    }
-
+	 /**
+	  * Sets a new grid.
+	  * @param grid the grid to be used
+	  */
 	 public void setGrid(Grid grid) {
 	        this.grid = grid;
 	    }
+	 
+	 
+	 /**
+	  * creates grid with wall and aperture consistency.
+	  * makes sure there is always one exit cell on the left most side.
+	  * 
+	  * @param size the size of the grid.
+	  * @return the generated grid. Or null if size was too big.
+	  */
 	 public Grid createRandomGrid(int size) {
 		 if (size < MinGrid || size > MaxGrid) {
 			 return null;
@@ -100,7 +133,12 @@ public class Game{
 		 
 		 return new Grid(rows);
 	 }
-	 
+	 /**
+		 * moves the player in the direction specified
+		 * @param move the movement direction
+		 * @param player the player that is moving
+		 * @return true if the movement worked, otherwise false
+		 */
 	 public boolean play(Movement move, Player player) {
 		    if (move == null || player == null) {
 		        return false;
@@ -113,7 +151,10 @@ public class Game{
 		    return result;
 		}
 
-	 
+	 /**
+	  *Returns a string of the game
+	  * @return a string describing the game
+	  */
 	 @Override
 	 public String toString() {
 		 return "Game [grid=" + grid + "]";
