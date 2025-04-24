4dde1d15aaec0cf0e109e9d2779df37785ff116c
Lilgibbss <134891354+Lilgibbss@users.noreply.github.com>
2025-04-23 20:11:20 -0500

    notes for row and javadoc

--- SP2025_CS3330_HW4_G0/src/hw4/maze/Row.java
+++ SP2025_CS3330_HW4_G0/src/hw4/maze/Row.java
@@ -3,20 +3,24 @@ package hw4.maze;
 import java.util.ArrayList;
 
 public class Row {
-
+	/*create a list for cells*/
 	private ArrayList<Cell> cells;
 	
+	/*each cell in the list is set to a cell in the row*/
 	public Row(ArrayList<Cell> cells) {
 		this.cells = cells;
 	}
 	
+	/*getter method for cells*/
 	public ArrayList<Cell> getCells() {
 		return cells;
 	}
 	
+	/*setter method for cells*/
 	public void setCells(ArrayList<Cell> cells) {
 		this.cells = cells;
 	}
+	
 	
 	@Override
 	public String toString() {
